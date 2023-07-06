91ea4f7822aaeda41e238d84b60a2cb06108eacb
reginamarinozzi <regina.marinozzi@gmail.com>
2023-07-05 22:30:01 -0300

    arreglos

--- Clase_001/index.js
+++ Clase_001/index.js
@@ -1,33 +1,35 @@
-class ProductManager{
-    constructor(){
-        this.products=[]
-    
+class ProductManager {
+    constructor() {
+        this.products = []
+
     }
-    static id=0
-    addProduct(title, description, price, thumbmail, code, stock){
+    static id = 0
+    addProduct(title, description, price, thumbmail, code, stock) {
         ProductManager.id++
-        this.products.push({title, description, price, thumbmail, code, stock, id:ProductManager.id});
+        this.products.push({ title, description, price, thumbmail, code, stock, id: ProductManager.id });
     }
 
-    getProduct(){
+    getProduct() {
         return this.products
     }
 
-    getProductById(id){
-        if(!this.products.find((product)=> product.id === id)){
+    getProductById(id) {
+        if (!this.products.find((product) => product.id === id)) {
             return 'Not Found'
-        }else{
-            return this.products.find((product)=> product.id === id)
+        } else {
+            return this.products.find((product) => product.id === id)
         }
     }
 }
- const productos = new ProductManager();
 
- console.log(productos.getProduct());
+const productos = new ProductManager();
 
- productos.addProduct('Remera','Rosa',11000,'img','aaaa1',4)
- productos.addProduct('Vestido','Azul',13000,'img2','bbbb2',6)
+console.log(productos.getProduct());
 
- console.log(productos.getProduct());
- console.log(productos.getProductById(2));
- console.log(productos.getProductById(3));
+productos.addProduct('Remera', 'Rosa', 11000, 'img', 'aaaa1', 4)
+productos.addProduct('Vestido', 'Azul', 13000, 'img2', 'bbbb2', 6)
+productos.addProduct('Camisa', 'Verde', 16000, 'img3', 'cccc3', 8)
+
+console.log(productos.getProduct());
+console.log(productos.getProductById(1));
+console.log(productos.getProductById(2));
